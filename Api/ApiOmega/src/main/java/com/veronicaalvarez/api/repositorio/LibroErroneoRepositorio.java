a62e9e7ee9595ef3c8979d93beecca9963f788a6
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:10:51 +0200

    Metodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/LibroErroneoRepositorio.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/LibroErroneoRepositorio.java
@@ -5,6 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Interfaz que proporciona métodos para interactuar con la base de datos de libros erróneos.
+ * 
+ * Esta interfaz extiende JpaRepository, lo que significa que hereda operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+ * comunes para la entidad LibroErroneo.
+ * 
+ * Además, se define un método personalizado para encontrar todos los libros erróneos no resueltos.
+ */
 public interface LibroErroneoRepositorio extends JpaRepository<LibroErroneo, Integer> {
+
+    /**
+     * Encuentra todos los libros erróneos que no estén resueltos.
+     * 
+     * @return una lista de libros erróneos no resueltos
+     */
     List<LibroErroneo> findByResueltoIsNull();
 }
