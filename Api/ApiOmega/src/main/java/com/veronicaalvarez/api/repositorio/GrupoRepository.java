133bdc8443851f3a1a672d6c0f0d652e1ec3ce3b
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:10:02 +0200

    Comentarios doc

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/GrupoRepository.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/GrupoRepository.java
@@ -4,6 +4,12 @@ import com.veronicaalvarez.api.modelo.Genero;
 import com.veronicaalvarez.api.modelo.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Interfaz que proporciona métodos para interactuar con la base de datos de grupos.
+ * 
+ * Esta interfaz extiende JpaRepository, lo que significa que hereda operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+ * comunes para la entidad Grupo.
+ */
 public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
 
 }
