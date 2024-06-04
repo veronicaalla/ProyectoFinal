547a613ca920603199ecc6818d5cee7bb5c8b749
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:16:37 +0200

    Metodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ValoracionLibroRepository.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ValoracionLibroRepository.java
@@ -5,6 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * Interfaz que proporciona métodos para interactuar con la base de datos de valoraciones de libros.
+ * 
+ * Esta interfaz extiende JpaRepository, lo que significa que hereda operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+ * comunes para la entidad ValoracionLibro.
+ * 
+ * Además, se define un método personalizado para encontrar una valoración de libro por ID de libro.
+ */
 public interface ValoracionLibroRepository extends JpaRepository<ValoracionLibro, Integer> {
+
+    /**
+     * Encuentra una valoración de libro específica por ID de libro.
+     * 
+     * @param idLibro el ID del libro al que pertenece la valoración
+     * @return una valoración de libro que coincida con el ID especificado, o un Optional vacío si no se encuentra
+     */
     Optional<ValoracionLibro> findByIdLibro(int idLibro);
 }
