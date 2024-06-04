e9abc458a0d5c9c404c8e1c87065e34553d74249
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:05:59 +0200

    Métodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/BibliotecaRepositorio.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/BibliotecaRepositorio.java
@@ -5,5 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.veronicaalvarez.api.modelo.Biblioteca;
 
 public interface BibliotecaRepositorio extends JpaRepository<Biblioteca, Integer> {
+
+    /**
+     * Busca bibliotecas asociadas con un usuario específico por su ID.
+     * 
+     * @param usuarioId ID del usuario asociado a las bibliotecas
+     * @return Lista de bibliotecas asociadas con el usuario
+     */
     List<Biblioteca> findByUsuario_Id(int usuarioId);
 }
