1bc5c761084da795b7f54b2af4e0c44b920d5eae
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:09:27 +0200

    Metodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/GeneroRepositorio.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/GeneroRepositorio.java
@@ -6,6 +6,19 @@ import com.veronicaalvarez.api.modelo.Genero;
 
 import java.util.Optional;
 
+/**
+ * Interfaz que proporciona métodos para interactuar con la base de datos de géneros.
+ * 
+ * Esta interfaz extiende JpaRepository, lo que significa que hereda operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+ * comunes para la entidad Genero.
+ */
 public interface GeneroRepositorio extends JpaRepository<Genero, Integer>{
+
+     /**
+     * Encuentra un género por su nombre.
+     * 
+     * @param nombre el nombre del género
+     * @return el género encontrado, o null si no se encuentra ninguno
+     */
     Genero findByNombre(String nombre);
 }
