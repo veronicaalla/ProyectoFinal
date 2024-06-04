0e145527f25b81aaca41c165cfa430143822d831
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:06:44 +0200

    Metodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ComentarioReportadoRepositorio.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ComentarioReportadoRepositorio.java
@@ -8,5 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.veronicaalvarez.api.modelo.ComentarioReportado;
 
 public interface ComentarioReportadoRepositorio extends JpaRepository <ComentarioReportado, Integer>{
+
+    /**
+     * Encuentra comentarios reportados que no tienen un valor "ofensivo".
+     * 
+     * @return Lista de comentarios reportados sin un valor "ofensivo"
+     */
     List<ComentarioReportado> findByOfensivoIsNull();
 }
