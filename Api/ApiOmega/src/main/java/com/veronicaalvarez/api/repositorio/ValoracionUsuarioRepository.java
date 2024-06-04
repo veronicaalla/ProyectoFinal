8ab8e8e2ff33ac1f7fe17a737f7434cac5407045
Verónica Álvarez <92816070+veronicaalla@users.noreply.github.com>
2024-06-04 13:17:47 +0200

    Métodos comentados

--- Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ValoracionUsuarioRepository.java
+++ Api/ApiOmega/src/main/java/com/veronicaalvarez/api/repositorio/ValoracionUsuarioRepository.java
@@ -5,6 +5,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+
+/**
+ * Interfaz que proporciona métodos para interactuar con la base de datos de valoraciones de usuarios.
+ * 
+ * Esta interfaz extiende JpaRepository, lo que significa que hereda operaciones CRUD (Crear, Leer, Actualizar, Eliminar)
+ * comunes para la entidad ValoracionUsuario.
+ * 
+ * Además, se define un método personalizado para encontrar una valoración de usuario específica por ID de libro y ID de usuario.
+ */
 public interface ValoracionUsuarioRepository extends JpaRepository<ValoracionUsuario, Integer> {
+
+     /**
+     * Encuentra una valoración de usuario específica por ID de libro y ID de usuario.
+     * 
+     * @param idLibro el ID del libro relacionado con la valoración
+     * @param idUsuario el ID del usuario que realizó la valoración
+     * @return una valoración de usuario que coincida con los IDs especificados, o un Optional vacío si no se encuentra
+     */
     Optional<ValoracionUsuario> findByLibroIdAndUsuarioId(int idLibro, int idUsuario);
 }
